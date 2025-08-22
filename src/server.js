c01fabf195b91ae8656815ede797386f425ce301
agathapadilha4-arch <agathapadilha4@gmail.com>
2025-08-22 17:20:28 -0300

    adicionando primeira versão do servidor

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/', (req,res) => {
-    res.send("olá mundo");
+    res.send("olá mundo!!");
 });
 app.listen(3000, ()=>
     console.log('Servidor rodando em http://localhost:3000')
