ded3230b2c3f8c8b5ab1267c2ed7608413083351
agathapadilha4-arch <agathapadilha4@gmail.com>
2025-08-22 17:33:46 -0300

    rota html

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const app = express();
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("olá mundo!!");
 });
-app.listen(3000, ()=>
+app.get('/html', (req, res) => {
+    res.send("<h1>teste</h1>");
+});
+app.listen(3000, () =>
     console.log('Servidor rodando em http://localhost:3000')
 );
